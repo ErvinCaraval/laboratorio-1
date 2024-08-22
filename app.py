3c118f156c64fe6c83877775f257b5e167afd5ab
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-08-21 20:00:22 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import redis
+
+app = FastAPI()
+
+class Item(BaseModel):
+    key: str
+    value: str
+
+## Redis Connection
+
+my_dict = {}
+r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+
+@app.get("/")
+async def root():
+    return {"owner": "Fredy Ballesteros"}
+
+@app.post("/add/")
+async def add_item(item: Item):
+    r.set(item.key, item.value)
+    return {"status": "Item added successfully"}
+
+@app.get("/get/{key}/")
+async def get_item(key: str):
+    value = r.get(key)
+    if value is None:
+        return {"Error": "Key not found"}
+    else:
+        return {"key": key, "value": value}
